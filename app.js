d9fa1af2b666ba83026eaf99e9a60b6846937564
Gabbiedee <akingbojudolly@gmail.com>
2023-03-27 01:16:07 +0100

    Create Hotel Site

---
+++ app.js
@@ -0,0 +1,7 @@
+const toggler = document.getElementById('toggler');
+const navLinks = document.querySelector('.nav-links2')
+
+toggler.addEventListener('click', ()=>{
+   navLinks.classList.toggle('active');
+console.log(1)
+});
